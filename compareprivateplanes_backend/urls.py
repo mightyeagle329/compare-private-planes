cbf01fa288d710c58d1cd4657ec2eb8a5aa5b1f8
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-01-18 15:58:17 +0100

    added import

--- compareprivateplanes_backend/urls.py
+++ compareprivateplanes_backend/urls.py
@@ -1,6 +1,6 @@
 from os import stat
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from django.conf.urls.static import static
 from django.conf import settings
@@ -9,5 +9,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_app.urls')),
-    # re_path('', TemplateView.as_view(template_name='index.html')),
+    re_path('', TemplateView.as_view(template_name='index.html')),
 ]
