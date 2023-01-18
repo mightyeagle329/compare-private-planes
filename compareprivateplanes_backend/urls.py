f92ea82958b7ff0ab918e155adfbc31bd7904e29
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-01-18 15:33:36 +0100

    added index.html as template

--- compareprivateplanes_backend/urls.py
+++ compareprivateplanes_backend/urls.py
@@ -8,4 +8,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_app.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
+
 ]
