34aab429dd622783529f090447f1c78911fb044f
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-01-23 13:22:03 +0100

    removed template

--- compareprivateplanes_backend/urls.py
+++ compareprivateplanes_backend/urls.py
@@ -9,5 +9,4 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_app.urls')),
-    re_path('', TemplateView.as_view(template_name='index.html')),
 ]
