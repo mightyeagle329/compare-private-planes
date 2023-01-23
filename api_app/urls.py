ec2ca5c441c7eaf796139f792347c9559c5ba1d2
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-01-23 13:35:17 +0100

    removed / from search url

--- api_app/urls.py
+++ api_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AircraftList, AircraftSearch
 
 urlpatterns = [
-    path("search/", AircraftSearch.as_view()),
+    path("search", AircraftSearch.as_view()),
     path("aircraft-items/", AircraftList.as_view()),
 ]
--- compareprivateplanes_backend/urls.py
+++ compareprivateplanes_backend/urls.py
@@ -1,12 +1,11 @@
-from os import stat
 from django.contrib import admin
 from django.urls import path, include, re_path
 
-from django.conf.urls.static import static
-from django.conf import settings
 from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api_app.urls')),
+    re_path('', TemplateView.as_view(template_name='index.html')),
 ]
