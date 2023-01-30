1a58e7f3e6e967b889e14a86615929aa678bc9f2
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-01-30 20:34:03 +0100

    added needed views

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import AircraftList, AircraftSearch
+from .views import AircraftList, AircraftSearch, AircraftById
 
 urlpatterns = [
     path("search", AircraftSearch.as_view()),
     path("aircraft-items/", AircraftList.as_view()),
+    path('aircrafts/<int:id>/', AircraftById.as_view()),
 ]
