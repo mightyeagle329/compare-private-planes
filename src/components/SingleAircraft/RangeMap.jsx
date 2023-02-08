b06d30f4ce681a39eb5309848d5b98baa6e27b39
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-02-08 19:16:08 +0100

    Update RangeMap.jsx

--- src/components/SingleAircraft/RangeMap.jsx
+++ src/components/SingleAircraft/RangeMap.jsx
@@ -58,6 +58,27 @@ function Map() {
       mapContainerStyle={{ height: 400 + "px" }}
     >
       <Marker position={position} />
+      <Autocomplete onLoad={this.onLoad} onPlaceChanged={this.onPlaceChanged}>
+        <input
+          type="text"
+          placeholder="Customized your placeholder"
+          style={{
+            boxSizing: `border-box`,
+            border: `1px solid transparent`,
+            width: `240px`,
+            height: `32px`,
+            padding: `0 12px`,
+            borderRadius: `3px`,
+            boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+            fontSize: `14px`,
+            outline: `none`,
+            textOverflow: `ellipses`,
+            position: "absolute",
+            left: "50%",
+            marginLeft: "-120px",
+          }}
+        />
+      </Autocomplete>
       <Circle center={center} options={options} />
       <p>chadi</p>
     </GoogleMap>
