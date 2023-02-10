44bba96b2c41f42fa6a7f4c91fb8061429c4b765
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-02-11 00:17:18 +0100

    Update views.py

--- api_app/views.py
+++ api_app/views.py
@@ -692,7 +692,7 @@ class AccidentsList(View):
         for item in all_accidents:
             accidents_data.append({
                 'country': item.country,
-                'aircraft_incident': item.aircraft_incident,
+                'aircraft_incident': item.aircraft_incident.aircraft_name,
                 'reg': item.reg,
                 'date': item.date,
                 'occurrence': item.occurrence,
