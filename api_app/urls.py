e9379d7e723f33b936fef41351e33e011974c8b3
Chadi Ayari <98648139+ayarichadi@users.noreply.github.com>
2023-03-13 14:06:54 +0100

    added upload csv file

--- api_app/urls.py
+++ api_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AircraftList, AircraftSearch, AircraftById, AccidentsList
+from .views import AircraftList, AircraftSearch, AircraftById, AccidentsList, upload_csv
 
 urlpatterns = [
     path("search", AircraftSearch.as_view()),
     path("aircraft-items/", AircraftList.as_view()),
     path("accident-items/", AccidentsList.as_view()),
     path('aircrafts/<int:id>/', AircraftById.as_view()),
+    path('upload-csv/', upload_csv, name="upload-csv"),
 ]
